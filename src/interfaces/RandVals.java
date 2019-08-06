9ded7c4cc499d63511c76aaad5197d483555a01b
GJXAIOU <gjx1690048420@163.com>
2019-08-06 13:55:54 +0800

    更新readme.md

--- src/interfaces/RandVals.java
+++ src/interfaces/RandVals.java
@@ -5,6 +5,7 @@ import java.util.*;
 
 public interface RandVals {
   Random RAND = new Random(47);
+  // 返回一个随机数，值均为分布介于0（含）- 10（不含）之间；
   int RANDOM_INT = RAND.nextInt(10);
   long RANDOM_LONG = RAND.nextLong() * 10;
   float RANDOM_FLOAT = RAND.nextLong() * 10;
